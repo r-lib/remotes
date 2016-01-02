93a56e7ecced74af8458c0663ea4dbff73f4e687
Gabor Csardi <csardi.gabor@gmail.com>
2016-01-02 02:32:47 +0000

    Implement download() for R >= 3.2.0

---
+++ R/download.R
@@ -0,0 +1,20 @@
+
+download <- function(path, url) {
+  download.file(url, path, method = download_method())
+}
+
+download_method <- function() {
+
+  if (.Platform$OS.type == "windows") {
+    "wininet"
+
+  } else if (isTRUE(unname(capabilities("libcurl")))) {
+    "libcurl"
+
+  } else if (Sys.which("wget") != "") {
+    "wget"
+
+  } else {
+    "auto"
+  }
+}
