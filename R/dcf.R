47d9fe8e596cc3f92a51fead38d14beda65f638e
Gabor Csardi <csardi.gabor@gmail.com>
2016-01-03 01:40:57 +0000

    Simplify write_dcf, just call write.dcf

--- R/dcf.R
+++ R/dcf.R
@@ -4,16 +4,10 @@ read_dcf <- function(path) {
 }
 
 write_dcf <- function(path, desc) {
-  desc <- unlist(desc)
-  # Add back in continuation characters
-  desc <- gsub("\n[ \t]*\n", "\n .\n ", desc, perl = TRUE, useBytes = TRUE)
-  desc <- gsub("\n \\.([^\n])", "\n  .\\1", desc, perl = TRUE, useBytes = TRUE)
-
-  text <- paste0(names(desc), ": ", desc, collapse = "\n")
-
-  if (substr(text, nchar(text), 1) != "\n") {
-    text <- paste0(text, "\n")
-  }
-
-  cat(text, file = path)
+  write.dcf(
+    rbind(unlist(desc)),
+    file = path,
+    keep.white = names(desc),
+    indent = 0
+  )
 }
