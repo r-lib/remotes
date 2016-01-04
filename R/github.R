45992d8cf54d85c9783ccea2f59f61447678487c
Gabor Csardi <csardi.gabor@gmail.com>
2016-01-04 16:49:18 +0000

    Remove jsonlite dep, with a crude JSON parser

--- R/github.R
+++ R/github.R
@@ -1,37 +1,17 @@
 
-github_auth <- function(token) {
-  if (is.null(token)) {
-    NULL
-  } else {
-    httr::authenticate(token, "x-oauth-basic", "basic")
-  }
-}
-
-github_response <- function(req) {
-  text <- httr::content(req, as = "text")
-  parsed <- jsonlite::fromJSON(text, simplifyVector = FALSE)
-
-  if (httr::status_code(req) >= 400) {
-    errors <- vapply(parsed$errors, `[[`, "message", FUN.VALUE = character(1))
-
-    stop(
-      parsed$message, " (", httr::status_code(req), ")\n",
-      paste("* ", errors, collapse = "\n"),
-      call. = FALSE
-    )
-  }
-
-  parsed
-}
-
-github_GET <- function(path, ..., pat = github_pat()) {
-  auth <- github_auth(pat)
-  req <- httr::GET("https://api.github.com/", path = path, auth, ...)
-  github_response(req)
-}
-
 github_commit <- function(username, repo, ref = "master") {
-  github_GET(file.path("repos", username, repo, "commits", ref))
+
+  url <- file.path("https://api.github.com",
+                   "repos", username, repo, "commits", ref)
+
+  pat <- github_pat()
+  auth <- if (is.null(pat)) "" else paste0("?access_code=", pat)
+  url <- paste0(url, auth)
+
+  tmp <- tempfile()
+  download(tmp, url)
+
+  json_dict_get(tmp, "sha")
 }
 
 #' Retrieve Github personal access token.
@@ -47,4 +27,78 @@ github_pat <- function() {
 
   message("Using github PAT from envvar GITHUB_PAT")
   pat
+}
+
+json_dict_get <- function(con, key) {
+  text <- paste(readLines(con, warn = FALSE), collapse = " ")
+  chr <- strsplit(text, "")[[1]]
+
+  in_string <- FALSE
+  in_topkey <- FALSE
+  escaped <- FALSE
+  level <- 0
+  keyword <- ""
+  for (i in seq_along(chr)) {
+    c <- chr[[i]]
+
+    ## Whatever is escaped, ignore it
+    if (escaped) {
+      escaped <- FALSE
+
+    ## Ignore escape characters
+    } else if (c == "\\") {
+      escaped <- TRUE
+
+    ## If in a top keyword that ends, maybe got it
+    } else if (in_topkey  && c == '"') {
+      if (keyword == key) { break }
+      keyword <- ""
+      in_topkey <- FALSE
+
+    ## Otherwise if in top keyword, collect it
+    } else if (in_topkey) {
+      keyword <- paste0(keyword, c)
+
+    ## If in a string, and ", the string is over
+    } else if (in_string && c == '"') {
+      in_string <- FALSE
+
+    ## Otherwise ignore the string
+    } else if (in_string) {
+
+    ## We are not in a string
+    ## Starting a string, maybe a top key
+    } else if (c == '"') {
+      if (level == 2) in_topkey <- TRUE else in_string <- TRUE
+
+    ## We go one level deeper, proper levels are even numbers
+    } else if (c == "{" || c == "[") {
+      level <- level + 2
+
+    ## We go one level higher
+    } else if (c == "}" || c == "]") {
+      level <- level - 2
+
+    ## A top level value is considered as an odd level
+    } else if (level == 2 && c == ":") {
+      level <- level + 1
+
+    ## We are in a top-level value
+    } else if (level == 3 && c == ",") {
+      level <- level - 1
+
+    ## just ignore other stuff (whitespace, mostly, some commas)
+    } else {
+
+    }
+  }
+
+  if (keyword != "sha") {
+    stop('Cannot get "sha" from GitHub', call. = FALSE)
+  }
+
+  ## Look for sha at position i
+  atsha <- paste(chr[i:length(chr)], collapse = "")
+
+  sub('^"\\s*:\\s*"([a-fA-F0-9]+)".*$', "\\1", atsha)
 }
