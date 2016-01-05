77f495ec175370dd5a24df21afa4f053b6bcf4ff
Gabor Csardi <csardi.gabor@gmail.com>
2016-01-05 21:23:03 +0000

    Remove some unused functionality

--- R/system.R
+++ R/system.R
@@ -2,24 +2,18 @@
 #'
 #' @param cmd the command to run.
 #' @param args a vector of command arguments.
-#' @param env a named character vector of environment variables.  Will be quoted
 #' @param quiet if \code{FALSE}, the command to be run will be echoed.
 #' @param ... additional arguments passed to \code{\link[base]{system}}
 #' @return \code{TRUE} if the command succeeds, an error will be thrown if the
 #' command fails.
 #' @noRd
-system_check <- function(cmd, args = character(), env = character(),
-                         quiet = FALSE, ...) {
+system_check <- function(cmd, args = character(), quiet = FALSE, ...) {
   full <- paste(shQuote(cmd), " ", paste(args, collapse = " "), sep = "")
 
   if (!quiet) {
     message(wrap_command(full))
     message()
   }
-
-  result <- suppressWarnings(with_envvar(env,
-    system(full, intern = quiet, ignore.stderr = quiet, ...)
-  ))
 
   if (quiet) {
     status <- attr(result, "status") %||% 0
--- R/utils.R
+++ R/utils.R
@@ -8,28 +8,3 @@ is_bioconductor <- function(x) {
 trim_ws <- function(x) {
   gsub("^[[:space:]]+|[[:space:]]+$", "", x)
 }
-
-with_envvar <- function (new, code) {
-  old <- set_envvar(envs = new)
-  on.exit(set_envvar(old))
-  force(code)
-}
-
-set_envvar <- function (envs) {
-  if (length(envs) == 0) return()
-
-  stopifnot(is.named(envs))
-
-  envs <- envs[!duplicated(names(envs), fromLast = TRUE)]
-  old <- Sys.getenv(names(envs), names = TRUE, unset = NA)
-  set <- !is.na(envs)
-  if (any(set))
-    do.call("Sys.setenv", as.list(envs[set]))
-  if (any(!set))
-    Sys.unsetenv(names(envs)[!set])
-  invisible(old)
-}
-
-is.named <- function (x) {
-  !is.null(names(x)) && all(names(x) != "")
-}
