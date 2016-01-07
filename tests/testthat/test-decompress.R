724980c249937af5de7f3900e46c148dad05c50b
Gabor Csardi <csardi.gabor@gmail.com>
2016-01-07 01:08:58 +0000

    Trying to fix source_pkg test case on windows

--- tests/testthat/test-decompress.R
+++ tests/testthat/test-decompress.R
@@ -72,16 +72,19 @@ test_that("source_pkg", {
   foo_dir <- system.file(package = packageName(), "archives", "foo")
   expect_equal(source_pkg(foo_dir), foo_dir)
 
-  foo_tgz <- system.file(package = packageName(), "archives", "foo.tar.gz")
-  pkg_dir <- source_pkg(foo_tgz)
-  on.exit(unlink(pkg_dir, recursive = TRUE), add = TRUE)
-  expect_true(file.exists(file.path(pkg_dir, "R", "foo.R")))
-  expect_true(file.exists(file.path(pkg_dir, "configure")))
-  expect_match(file.info(file.path(pkg_dir, "configure"))$mode, "7..")
-
   bad_dir <- system.file(package = packageName(), "archives")
   expect_error(
     source_pkg(bad_dir),
     "Does not appear to be an R package"
   )
+
+  foo_tgz <- system.file(package = packageName(), "archives", "foo.tar.gz")
+  pkg_dir <- source_pkg(foo_tgz)
+  on.exit(unlink(pkg_dir, recursive = TRUE), add = TRUE)
+  expect_true(file.exists(file.path(pkg_dir, "R", "foo.R")))
+  expect_true(file.exists(file.path(pkg_dir, "configure")))
+
+  skip_on_os("windows")
+
+  expect_match(file.info(file.path(pkg_dir, "configure"))$mode, "7..")
 })
