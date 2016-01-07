96b1da48bd0d96c5b1157e3daebaf07845f0d41b
Gabor Csardi <csardi.gabor@gmail.com>
2016-01-07 00:09:44 +0000

    More install_github tests

--- tests/testthat/test-install-github.R
+++ tests/testthat/test-install-github.R
@@ -21,3 +21,64 @@ test_that("install_github", {
   expect_equal(packageDescription("falsy")$RemoteRepo, "falsy")
 
 })
+
+test_that("error if not username, warning if given as argument", {
+
+  skip_on_cran()
+  skip_if_offline()
+  skip_if_over_rate_limit()
+
+  Sys.unsetenv("R_TESTS")
+
+  lib <- tempfile()
+  on.exit(unlink(lib, recursive = TRUE), add = TRUE)
+  dir.create(lib)
+
+  expect_error(
+    install_github("falsy", lib = lib, quiet = TRUE),
+    "Unknown username"
+  )
+
+  expect_warning(
+    install_github("falsy", username = "cran", lib = lib, quiet = TRUE),
+    "Username parameter is deprecated"
+  )
+})
+
+test_that("remote_download.github_remote", {
+
+  with_mock(
+    `remotes::github_has_submodules` = function(...) TRUE,
+    `remotes::download` = function(...) TRUE,
+    expect_warning(
+      remote_download.github_remote(
+        list(
+          host = "api.github.com",
+          username = "cran",
+          repo = "falsy",
+          ref = "master"
+        )
+      ),
+      "GitHub repo contains submodules"
+    )
+  )
+})
+
+test_that("remote_download.github_remote messages", {
+
+  with_mock(
+    `remotes::github_has_submodules` = function(...) FALSE,
+    `remotes::download` = function(...) TRUE,
+    expect_message(
+      remote_download.github_remote(
+        list(
+          host = "api.github.com",
+          username = "cran",
+          repo = "falsy",
+          ref = "master"
+        )
+      ),
+      "Downloading GitHub repo"
+    )
+  )
+})
