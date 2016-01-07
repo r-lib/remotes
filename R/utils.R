cebac6f443cad3e830a9ea34e544d368664a7f24
Gabor Csardi <csardi.gabor@gmail.com>
2016-01-07 13:33:30 +0000

    with_envvar added from devtools

--- R/utils.R
+++ R/utils.R
@@ -8,3 +8,47 @@ is_bioconductor <- function(x) {
 trim_ws <- function(x) {
   gsub("^[[:space:]]+|[[:space:]]+$", "", x)
 }
+
+with_something <- function(set, reset = set) {
+  function(new, code) {
+    old <- set(new)
+    on.exit(reset(old))
+    force(code)
+  }
+}
+
+
+set_envvar <- function(envs, action = "replace") {
+  if (length(envs) == 0) return()
+
+  stopifnot(is.named(envs))
+  stopifnot(is.character(action), length(action) == 1)
+  action <- match.arg(action, c("replace", "prefix", "suffix"))
+
+  old <- Sys.getenv(names(envs), names = TRUE, unset = NA)
+  set <- !is.na(envs)
+
+  both_set <- set & !is.na(old)
+  if (any(both_set)) {
+    if (action == "prefix") {
+      envs[both_set] <- paste(envs[both_set], old[both_set])
+    } else if (action == "suffix") {
+      envs[both_set] <- paste(old[both_set], envs[both_set])
+    }
+  }
+
+  if (any(set))  do.call("Sys.setenv", as.list(envs[set]))
+  if (any(!set)) Sys.unsetenv(names(envs)[!set])
+
+  invisible(old)
+}
+
+with_envvar <- function(new, code, action = "replace") {
+  old <- set_envvar(new, action)
+  on.exit(set_envvar(old, "replace"))
+  force(code)
+}
+
+is.named <- function(x) {
+  !is.null(names(x)) && all(names(x) != "")
+}
