6f3163511aad70f1e36c4eafbeec83c3c6321574
Gabor Csardi <csardi.gabor@gmail.com>
2016-01-07 13:52:44 +0000

    with_envvar: no need for action argument

--- R/utils.R
+++ R/utils.R
@@ -9,24 +9,15 @@ trim_ws <- function(x) {
   gsub("^[[:space:]]+|[[:space:]]+$", "", x)
 }
 
-set_envvar <- function(envs, action = "replace") {
+set_envvar <- function(envs) {
   if (length(envs) == 0) return()
 
   stopifnot(is.named(envs))
-  stopifnot(is.character(action), length(action) == 1)
-  action <- match.arg(action, c("replace", "prefix", "suffix"))
 
   old <- Sys.getenv(names(envs), names = TRUE, unset = NA)
   set <- !is.na(envs)
 
   both_set <- set & !is.na(old)
-  if (any(both_set)) {
-    if (action == "prefix") {
-      envs[both_set] <- paste(envs[both_set], old[both_set])
-    } else if (action == "suffix") {
-      envs[both_set] <- paste(old[both_set], envs[both_set])
-    }
-  }
 
   if (any(set))  do.call("Sys.setenv", as.list(envs[set]))
   if (any(!set)) Sys.unsetenv(names(envs)[!set])
@@ -34,9 +25,9 @@ set_envvar <- function(envs, action = "replace") {
   invisible(old)
 }
 
-with_envvar <- function(new, code, action = "replace") {
-  old <- set_envvar(new, action)
-  on.exit(set_envvar(old, "replace"))
+with_envvar <- function(new, code) {
+  old <- set_envvar(new)
+  on.exit(set_envvar(old))
   force(code)
 }
 
