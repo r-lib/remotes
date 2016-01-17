7c23283844b694bd1c712faa41032affc4901013
Gabor Csardi <csardi.gabor@gmail.com>
2016-01-17 13:58:19 +0000

    Test case for install_svn and subdir argument

--- tests/testthat/test-install-svn.R
+++ tests/testthat/test-install-svn.R
@@ -45,3 +45,30 @@ test_that("install_svn branch", {
   expect_equal(packageDescription("simplegraph")$RemoteType, "svn")
 
 })
+
+test_that("install_svn subdir", {
+
+  skip_on_cran()
+  skip_if_offline()
+
+  Sys.unsetenv("R_TESTS")
+
+  lib <- tempfile()
+  on.exit(unlink(lib, recursive = TRUE), add = TRUE)
+  dir.create(lib)
+  libpath <- .libPaths()
+  on.exit(.libPaths(libpath), add = TRUE)
+  .libPaths(lib)
+
+  with_mock(
+    `remotes::install` = function(dir, ...) { print(list.files(dir)); TRUE },
+    expect_output(
+      install_svn(
+        "https://github.com/dmlc/xgboost",
+        subdir = "R-package"
+      ),
+      "DESCRIPTION"
+    )
+  )
+})
+
