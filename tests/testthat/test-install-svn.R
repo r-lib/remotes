0e7f52e9206b9d6e79f77bc1bd4c2483c06fc76a
Gabor Csardi <csardi.gabor@gmail.com>
2016-01-17 13:26:47 +0000

    SVN branch test case

--- tests/testthat/test-install-svn.R
+++ tests/testthat/test-install-svn.R
@@ -21,3 +21,27 @@ test_that("install_svn", {
   expect_equal(packageDescription("simplegraph")$RemoteType, "svn")
 
 })
+
+test_that("install_svn branch", {
+
+  skip_on_cran()
+  skip_if_offline()
+
+  Sys.unsetenv("R_TESTS")
+
+  lib <- tempfile()
+  on.exit(unlink(lib, recursive = TRUE), add = TRUE)
+  dir.create(lib)
+  libpath <- .libPaths()
+  on.exit(.libPaths(libpath), add = TRUE)
+  .libPaths(lib)
+
+  install_svn(
+    "https://github.com/mangothecat/simplegraph",
+    branch = "remotes-test"
+  )
+
+  expect_silent(packageDescription("simplegraph"))
+  expect_equal(packageDescription("simplegraph")$RemoteType, "svn")
+
+})
