d255b90d89571750e30d3c9d1dcf7bf3c69feb9a
Jim Hester <james.f.hester@gmail.com>
2018-08-02 15:36:14 -0400

    Move tests to match source filenames

---
+++ tests/testthat/test-install-remote.R
@@ -0,0 +1,41 @@
+context("install-remotes.R")
+
+test_that("different_sha returns TRUE if remote or local sha is NA not found", {
+  expect_true(different_sha(remote_sha = NA, local_sha = "4a2ea2"))
+  expect_true(different_sha(remote_sha = "4a2ea2", local_sha = NA))
+  expect_true(different_sha(remote_sha = NA, local_sha = NA))
+})
+
+test_that("different_sha returns TRUE if remote_sha and local_sha are different", {
+  expect_true(different_sha(remote_sha = "5b3fb3", local_sha = "4a2ea2"))
+})
+
+test_that("different_sha returns FALSE if remote_sha and local_sha are the same", {
+  expect_false(different_sha(remote_sha = "4a2ea2", local_sha = "4a2ea2"))
+})
+
+test_that("local_sha returns NA if package is not installed", {
+  expect_equal(local_sha("tsrtarst"), NA_character_)
+})
+
+test_that("remote_sha.github_remote errors if remote doesn't exist", {
+  expect_error(remote_sha(github_remote("arst/arst")), "cannot open URL")
+})
+
+test_that("remote_sha.github_remote returns expected value if remote does exist", {
+  expect_equal(remote_sha(github_remote("r-lib/devtools@v1.8.0")), "ad9aac7b9a522354e1ff363a86f389e32cec181b")
+})
+
+test_that("package2remotes looks for the DESCRIPTION in .libPaths", {
+   expect_equal(package2remote("noremotes")$sha, NA_character_)
+   withr::with_temp_libpaths({
+     expect_equal(package2remote("noremotes")$sha, NA_character_)
+     install("noremotes", quiet = TRUE)
+     expect_equal(package2remote("noremotes")$sha, "1.0.0")
+
+     # Load the namespace, as packageDescription looks in loaded namespaces
+     # first.
+     loadNamespace("testTest")
+    })
+  expect_equal(package2remote("noremotes")$sha, NA_character_)
+})
