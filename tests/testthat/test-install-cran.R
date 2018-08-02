e570c4d9582437e46703611ea64f47184351af62
Jim Hester <james.f.hester@gmail.com>
2018-08-02 15:44:42 -0400

    Test simple installation from CRAN

---
+++ tests/testthat/test-install-cran.R
@@ -0,0 +1,26 @@
+context("Installing from CRAN")
+
+test_that("", {
+
+  skip_on_cran()
+  skip_if_offline()
+
+  Sys.unsetenv("R_TESTS")
+
+  lib <- tempfile()
+  on.exit(unlink(lib, recursive = TRUE), add = TRUE)
+  dir.create(lib)
+  libpath <- .libPaths()
+  on.exit(.libPaths(libpath), add = TRUE)
+  .libPaths(lib)
+
+  install_cran("pkgconfig", lib = lib, force = TRUE)
+
+  expect_silent(packageDescription("pkgconfig", lib.loc = lib))
+  expect_equal(
+    packageDescription("pkgconfig", lib.loc = lib)$RemoteType,
+    NULL)
+  expect_equal(
+    packageDescription("pkgconfig", lib.loc = lib)$RemoteUrl,
+    NULL)
+})
