496787cfc4b0ff34f6622d3a0a42baa12c57f505
Jim Hester <james.f.hester@gmail.com>
2018-08-27 09:36:54 -0400

    Remove BioCextra repository
    This was removed in R 3.5+ / Bioc 3.6+
    
    https://www.mail-archive.com/bioc-devel@r-project.org/msg07368.html

--- tests/testthat/test-bioc.R
+++ tests/testthat/test-bioc.R
@@ -39,11 +39,27 @@ test_that("bioc_install_repos", {
     "Unsupported"
   )
 
+  # This particular version needs to do a connection test for https support
   skip_if_offline()
   skip_on_cran()
   expect_equal(
     bioc_install_repos("3.2.2"),
     bioc_repos("3.2")
+  )
+})
+
+test_that("CRANextras exists in versions prior to Bioc 3.6", {
+  # BioCextra was removed in R 3.5
+  skip_if(getRversion() >= "3.5")
+
+  expect_equal(
+    names(bioc_repos("3.5")),
+    c("BioCsoft", "BioCann", "BioCexp", "BioCextra")
+  )
+
+  expect_equal(
+    names(bioc_repos("3.6")),
+    c("BioCsoft", "BioCann", "BioCexp")
   )
 })
 
