09a3c376acc452cf87cbd04f167d553622e99cf0
Jim Hester <james.f.hester@gmail.com>
2018-08-28 08:55:22 -0400

    Remove git_extract_sha1_zip
    It is now unused, as we always download tarballs instead

--- tests/testthat/test-git.R
+++ tests/testthat/test-git.R
@@ -1,28 +1,6 @@
 
 context("Git")
 
-test_that("git_extract_sha1_zip", {
-
-  skip_on_cran()
-  skip_if_offline()
-  skip_if_over_rate_limit()
-
-  sha <- "fbae60ced0afee0e7c0f8dc3b5b1bb48d303f3dd"
-  url <- paste0(
-    "https://api.github.com/repos/hadley/devtools/zipball/",
-    sha
-  )
-
-  tmp <- tempfile()
-  on.exit(unlink(tmp), add = TRUE)
-  download(tmp, url, auth_token = github_pat())
-
-  expect_equal(
-    git_extract_sha1_zip(tmp),
-    sha
-  )
-})
-
 test_that("git_extract_sha1_tar", {
 
   skip_on_cran()
