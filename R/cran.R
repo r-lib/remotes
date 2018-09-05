5d2f9201c2c75fbb81d1f4474d8643a63658f1a2
Jim Hester <james.f.hester@gmail.com>
2018-09-05 17:07:47 -0400

    Cache available.packages
    This function takes a substantial amount of time to run if not cached.
    It is called in remote_sha.cran_remote, which makes `package_deps()`
    take an very long time to run otherwise.

--- R/cran.R
+++ R/cran.R
@@ -1,4 +1,12 @@
 
-available_packages <- function(repos, type) {
-  suppressWarnings(utils::available.packages(utils::contrib.url(repos, type), type = type))
-}
+available_packages <- local({
+  cache <- new.env(emptyenv())
+
+  function(repos, type) {
+    signature <- rawToChar(serialize(1:10, NULL, ascii = TRUE))
+    if (is.null(cache[[signature]])) {
+      cache[[signature]] <- suppressWarnings(utils::available.packages(utils::contrib.url(repos, type), type = type))
+    }
+    cache[[signature]]
+  }
+})
