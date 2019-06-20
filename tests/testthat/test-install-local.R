0d4f02705234f280eaf7216e8abbe6fe5eac23ba
Jim Hester <james.f.hester@gmail.com>
2019-06-20 16:03:30 -0400

    Fix libPaths setting

--- tests/testthat/test-install-local.R
+++ tests/testthat/test-install-local.R
@@ -62,7 +62,8 @@ test_that("local remotes do not fail if the local install no longer exists", {
 
   unlink(dir, recursive = TRUE)
 
-  old <- .libPaths(lib)
+  old <- .libPaths()
+  .libPaths(lib)
   on.exit(.libPaths(old), add = TRUE)
 
   expect_error(update_packages(pkg[, 1], lib = lib, upgrade = TRUE, quiet = TRUE), NA)
