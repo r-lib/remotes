7af803a500018ef3184701e0882aa007da77d519
Jim Hester <james.f.hester@gmail.com>
2019-06-24 12:10:58 -0400

    Always follow redirects when downloading using external curl
    By default external curl does not follow redirects, but GitHub uses 302
    redirects extensively, causing downloads to fail.
    
    Fixes #350

--- tests/testthat/test-download.R
+++ tests/testthat/test-download.R
@@ -281,3 +281,20 @@ test_that("curl download with basic auth", {
   expect_true(resp$authenticated)
   expect_equal(resp$user, "ruser")
 })
+
+test_that("base curl download redirects", {
+  skip_on_cran()
+  skip_if_offline()
+  skip_without_program("curl")
+
+  url <- "http://httpbin.org/absolute-redirect/1"
+  tmp <- tempfile()
+  on.exit(unlink(tmp), add = TRUE)
+  with_options(
+    list(download.file.method = "curl"),
+         download(url, path = tmp, quiet = TRUE)
+  )
+  expect_true(file.exists(tmp))
+  resp <- json$parse(readLines(tmp))
+  expect_equal(resp$url, "https://httpbin.org/get")
+})
