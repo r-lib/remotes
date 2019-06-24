0c980e2cfbbb9cbdd6d8b515069fad30140b008e
Gábor Csárdi <csardi.gabor@gmail.com>
2019-06-24 16:19:00 +0100

    Send auth information in the headers (#391)
    * Send auth information in the headers
    
    Instead of sending it in query parameters.
    This way it is not printed in the logs on errors.
    
    Also refactored the download code a bit. We still respect
    the user's `download.file.method` option.
    
    We have tests now for the various download methods.
    
    Closes #364.
    
    * Fix download test if user has method set
    
    We need to unset the `download.file.method`
    while running the tests, in case the user
    running the tests has a default set.
    
    * Update install-github.R file
    
    * Fix download tests on windows
    
    Need to double quote, single quote is
    not special on Windows.
    
    * Warn if cannot send HTTP headers
    
    Older R versions cannot send headers with
    the wininet method.
    
    * Update install-github.R file
    
    * Add note to NEWS
    
    [ci skip]

--- tests/testthat/test-download.R
+++ tests/testthat/test-download.R
@@ -5,21 +5,25 @@ test_that("download_method", {
 
   mockery::stub(download_method, "get_r_version", "3.3.0")
   mockery::stub(download_method, "has_curl", FALSE)
-  expect_equal(download_method(), "auto")
+  with_options(list(download.file.method = NULL),
+               expect_equal(download_method(), "auto"))
 
   mockery::stub(download_method, "get_r_version", "3.2.5")
   mockery::stub(download_method, "os_type", "windows")
-  expect_equal(download_method(), "wininet")
+  with_options(list(download.file.method = NULL),
+               expect_equal(download_method(), "wininet"))
 
   mockery::stub(download_method, "get_r_version", "3.2.5")
   mockery::stub(download_method, "os_type", "unix")
   mockery::stub(download_method, "has_curl", TRUE)
-  expect_equal(download_method(), "libcurl")
+  with_options(list(download.file.method = NULL),
+               expect_equal(download_method(), "libcurl"))
 
   mockery::stub(download_method, "get_r_version", "3.2.5")
   mockery::stub(download_method, "os_type", "unix")
   mockery::stub(download_method, "has_curl", FALSE)
-  expect_equal(download_method(), "auto")
+  with_options(list(download.file.method = NULL),
+               expect_equal(download_method(), "auto"))
 })
 
 test_that("download", {
@@ -32,11 +36,11 @@ test_that("download", {
 
   download(tmp, "http://httpbin.org/get", auth_token = NULL)
   res <- json$parse_file(tmp)
-  expect_true("args" %in% names(res))
+  expect_null(res$headers$Authorization)
 
   download(tmp, "http://httpbin.org/get", auth_token = "foobar")
   res <- json$parse_file(tmp)
-  expect_equal(res$args$access_token, "foobar")
+  expect_equal(res$headers$Authorization, "token foobar")
 
 })
 
@@ -88,6 +92,78 @@ test_that("base download with custom headers", {
   expect_equal(resp$headers$`X-Custom`, "Foobar")
 })
 
+test_that("wget method download with custom headers", {
+  skip_on_cran()
+  skip_if_offline()
+  skip_without_program("wget")
+
+  url <- "http://httpbin.org/anything"
+  tmp <- tempfile()
+  on.exit(unlink(tmp), add = TRUE)
+  head <- c("X-Custom" = "Foobar")
+  extra <- "--header=\"X-Another: extra-header\""
+  with_options(
+    list(download.file.method = "wget", download.file.extra = extra),
+    base_download(url, path = tmp, quiet = TRUE, headers = head))
+  expect_true(file.exists(tmp))
+  resp <- json$parse(readLines(tmp))
+  expect_equal(resp$headers$`X-Custom`, "Foobar")
+  expect_equal(resp$headers$`X-Another`, "extra-header")
+})
+
+test_that("curl method download with custom headers", {
+  skip_on_cran()
+  skip_if_offline()
+  skip_without_program("curl")
+
+  url <- "http://httpbin.org/anything"
+  tmp <- tempfile()
+  on.exit(unlink(tmp), add = TRUE)
+  head <- c("X-Custom" = "Foobar")
+  extra <- "-H \"X-Another: extra-header\""
+  with_options(
+    list(download.file.method = "curl", download.file.extra = extra),
+    base_download(url, path = tmp, quiet = TRUE, headers = head))
+  expect_true(file.exists(tmp))
+  resp <- json$parse(readLines(tmp))
+  expect_equal(resp$headers$`X-Custom`, "Foobar")
+  expect_equal(resp$headers$`X-Another`, "extra-header")
+})
+
+test_that("internal method download with custom headers", {
+  skip_on_cran()
+  skip_if_offline()
+
+  url <- "http://httpbin.org/anything"
+  tmp <- tempfile()
+  on.exit(unlink(tmp), add = TRUE)
+  head <- c("X-Custom" = "Foobar")
+  with_options(
+    list(download.file.method = "internal"),
+    base_download(url, path = tmp, quiet = TRUE, headers = head))
+  expect_true(file.exists(tmp))
+  resp <- json$parse(readLines(tmp))
+  expect_equal(resp$headers$`X-Custom`, "Foobar")
+})
+
+test_that("wininet method download with custom headers", {
+  skip_on_cran()
+  skip_if_offline()
+  if (os_type() == "unix") return(expect_true(TRUE))
+  if (getRversion() < "3.6.0") return(expect_true(TRUE))
+
+  url <- "http://httpbin.org/anything"
+  tmp <- tempfile()
+  on.exit(unlink(tmp), add = TRUE)
+  head <- c("X-Custom" = "Foobar")
+  with_options(
+    list(download.file.method = "wininet"),
+    base_download(url, path = tmp, quiet = TRUE, headers = head))
+  expect_true(file.exists(tmp))
+  resp <- json$parse(readLines(tmp))
+  expect_equal(resp$headers$`X-Custom`, "Foobar")
+})
+
 test_that("curl download with custom headers", {
   skip_on_cran()
   skip_if_offline()
@@ -111,6 +187,78 @@ test_that("base download with basic auth", {
   on.exit(unlink(tmp), add = TRUE)
   download(url, path = tmp, quiet = TRUE,
            basic_auth = list(user = "ruser", password = "rpass"))
+  expect_true(file.exists(tmp))
+  resp <- json$parse(readLines(tmp))
+  expect_true(resp$authenticated)
+  expect_equal(resp$user, "ruser")
+})
+
+test_that("base wget download with basic auth", {
+  skip_on_cran()
+  skip_if_offline()
+  skip_without_program("wget")
+
+  url <- "http://httpbin.org/basic-auth/ruser/rpass"
+  tmp <- tempfile()
+  on.exit(unlink(tmp), add = TRUE)
+  with_options(
+    list(download.file.method = "wget"),
+         download(url, path = tmp, quiet = TRUE,
+                  basic_auth = list(user = "ruser", password = "rpass")))
+  expect_true(file.exists(tmp))
+  resp <- json$parse(readLines(tmp))
+  expect_true(resp$authenticated)
+  expect_equal(resp$user, "ruser")
+})
+
+test_that("base curl download with basic auth", {
+  skip_on_cran()
+  skip_if_offline()
+  skip_without_program("curl")
+
+  url <- "http://httpbin.org/basic-auth/ruser/rpass"
+  tmp <- tempfile()
+  on.exit(unlink(tmp), add = TRUE)
+  with_options(
+    list(download.file.method = "curl"),
+         download(url, path = tmp, quiet = TRUE,
+                  basic_auth = list(user = "ruser", password = "rpass")))
+  expect_true(file.exists(tmp))
+  resp <- json$parse(readLines(tmp))
+  expect_true(resp$authenticated)
+  expect_equal(resp$user, "ruser")
+})
+
+test_that("base internal download with basic auth", {
+  skip_on_cran()
+  skip_if_offline()
+
+  url <- "http://httpbin.org/basic-auth/ruser/rpass"
+  tmp <- tempfile()
+  on.exit(unlink(tmp), add = TRUE)
+  with_options(
+    list(download.file.method = "internal"),
+         download(url, path = tmp, quiet = TRUE,
+                  basic_auth = list(user = "ruser", password = "rpass")))
+  expect_true(file.exists(tmp))
+  resp <- json$parse(readLines(tmp))
+  expect_true(resp$authenticated)
+  expect_equal(resp$user, "ruser")
+})
+
+test_that("base wininet download with basic auth", {
+  skip_on_cran()
+  skip_if_offline()
+  if (os_type() == "unix") return(expect_true(TRUE))
+  if (getRversion() < "3.6.0") return(expect_true(TRUE))
+
+  url <- "http://httpbin.org/basic-auth/ruser/rpass"
+  tmp <- tempfile()
+  on.exit(unlink(tmp), add = TRUE)
+  with_options(
+    list(download.file.method = "wininet"),
+         download(url, path = tmp, quiet = TRUE,
+                  basic_auth = list(user = "ruser", password = "rpass")))
   expect_true(file.exists(tmp))
   resp <- json$parse(readLines(tmp))
   expect_true(resp$authenticated)
