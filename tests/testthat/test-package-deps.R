384ea556438cbfb2ab4a9ada6188e8fdf61f6bd8
Jim Hester <james.f.hester@gmail.com>
2020-07-13 11:32:41 -0400

    Run styler on test-package-deps

--- tests/testthat/test-package-deps.R
+++ tests/testthat/test-package-deps.R
@@ -2,7 +2,6 @@
 context("Package dependencies")
 
 test_that("parse_deps", {
-
   expect_null(parse_deps(NULL))
   expect_null(parse_deps(""))
   expect_null(parse_deps("  "))
@@ -14,7 +13,8 @@ test_that("parse_deps", {
       list(
         name = "devtools",
         compare = ">=",
-        version = "1.0.1"),
+        version = "1.0.1"
+      ),
       row.names = 1L,
       class = "data.frame"
     )
@@ -26,7 +26,8 @@ test_that("parse_deps", {
       list(
         name = c("devtools", "foobar", "foobar2"),
         compare = c(">=", NA, "=="),
-        version = c("1.0.1", NA, "0.0.1")),
+        version = c("1.0.1", NA, "0.0.1")
+      ),
       row.names = 1:3,
       class = "data.frame"
     )
@@ -39,7 +40,8 @@ test_that("parse_deps", {
       list(
         name = c("devtools", "foobar", "foobar2"),
         compare = c(">=", NA, "=="),
-        version = c("1.0.1", NA, "0.0.1")),
+        version = c("1.0.1", NA, "0.0.1")
+      ),
       row.names = 1:3,
       class = "data.frame"
     )
@@ -51,7 +53,8 @@ test_that("parse_deps", {
       list(
         name = c("package", "package2"),
         compare = c(">=", "<"),
-        version = c("1.0.1", "0.0.1")),
+        version = c("1.0.1", "0.0.1")
+      ),
       row.names = 1:2,
       class = "data.frame"
     )
@@ -64,41 +67,41 @@ test_that("version requirement comparisons", {
   required2 <- "< 2.1, > 1.5"
 
   for (required in list(required1, required2)) {
-    expect_true(version_satisfies_criteria('2.0', required), label = required)
+    expect_true(version_satisfies_criteria("2.0", required), label = required)
     expect_true(version_satisfies_criteria(NULL, required))
-    expect_false(version_satisfies_criteria('2.1', required))
-    expect_false(version_satisfies_criteria('1.5', required))
+    expect_false(version_satisfies_criteria("2.1", required))
+    expect_false(version_satisfies_criteria("1.5", required))
 
     mockery::stub(package_installed, "packageDescription", function(...) "2.0")
-    expect_true(package_installed('foo', required))
+    expect_true(package_installed("foo", required))
 
     mockery::stub(package_installed, "packageDescription", function(...) "1.0")
-    expect_false(package_installed('foo', required))
+    expect_false(package_installed("foo", required))
   }
 
   expect_equal(
-    version_criteria('1.5'),
-    data.frame(compare='==', version='1.5', stringsAsFactors=FALSE)
+    version_criteria("1.5"),
+    data.frame(compare = "==", version = "1.5", stringsAsFactors = FALSE)
   )
 
   expect_equivalent(
     version_criteria(NULL),
-    data.frame(compare=NA_character_, version=NA_character_, stringsAsFactors=FALSE)
+    data.frame(compare = NA_character_, version = NA_character_, stringsAsFactors = FALSE)
   )
 
   expect_equivalent(
     version_criteria(NA),
-    data.frame(compare=NA_character_, version=NA_character_, stringsAsFactors=FALSE)
+    data.frame(compare = NA_character_, version = NA_character_, stringsAsFactors = FALSE)
   )
 
   expect_equal(
-    version_criteria(c('> 1.5, < 2.0')),
-    data.frame(compare=c('>', '<'), version=c('1.5', '2.0'), stringsAsFactors=FALSE)
+    version_criteria(c("> 1.5, < 2.0")),
+    data.frame(compare = c(">", "<"), version = c("1.5", "2.0"), stringsAsFactors = FALSE)
   )
 
   expect_equal(
-    version_criteria(c('> 1.5', '< 2.0')),
-    data.frame(compare=c('>', '<'), version=c('1.5', '2.0'), stringsAsFactors=FALSE)
+    version_criteria(c("> 1.5", "< 2.0")),
+    data.frame(compare = c(">", "<"), version = c("1.5", "2.0"), stringsAsFactors = FALSE)
   )
 
   expect_equal(
@@ -120,7 +123,6 @@ test_that("version requirement comparisons", {
 
 
 test_that("parse_deps errors", {
-
   expect_error(parse_deps(42), "is.character.*is not TRUE")
 
   expect_error(
@@ -132,19 +134,18 @@ test_that("parse_deps errors", {
     parse_deps("remotes (>=1.0.0)"),
     "Invalid comparison operator in dependency"
   )
-
 })
 
 
 test_that("parse_deps omits R", {
-
   expect_equal(
     parse_deps("R (>= 2.15.3)"),
     structure(
       list(
         name = character(0),
         compare = character(0),
-        version = character(0)),
+        version = character(0)
+      ),
       row.names = integer(0),
       class = "data.frame"
     )
@@ -156,7 +157,8 @@ test_that("parse_deps omits R", {
       list(
         name = "devtools",
         compare = ">=",
-        version = "1.0.1"),
+        version = "1.0.1"
+      ),
       row.names = 2L,
       class = "data.frame"
     )
@@ -168,10 +170,10 @@ test_that("parse_deps omits R", {
       list(
         name = "devtools",
         compare = ">=",
-        version = "1.0.1"),
+        version = "1.0.1"
+      ),
       row.names = 1L,
       class = "data.frame"
     )
   )
-
 })
