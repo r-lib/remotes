7586e50c26aa245bbf52824d9269dfe3b177c48c
Tom Shafer <tom.shafer@elderresearch.com>
2020-08-19 10:43:42 -0400

    Use Artifactory API

--- R/install-artifactory.R
+++ R/install-artifactory.R
@@ -1,37 +1,18 @@
-
-# What do we need for artifactory?
-# user name
-# API key
-# URL
-# repo
-
-# Only support source installs for V1
-
-#' Attempts to install a package directly from GitHub.
+#' Attempt to install package(s) from an Artifactory repo.
 #'
-#' This function is vectorised on `repo` so you can install multiple
-#' packages in a single command.
+#' This function is vectorised on `pkgs` to allow installing
+#' multiple packages with a single command.
 #'
-#' @param pkgs Repository address in the format
-#'   `username/repo[/subdir][@@ref|#pull|@@*release]`. Alternatively, you can
-#'   specify `subdir` and/or `ref` using the respective parameters
-#'   (see below); if both are specified, the values in `repo` take
-#'   precedence.
-#' @param auth_token To install from a private repo, generate a personal
-#'   access token (PAT) in "https://github.com/settings/tokens" and
-#'   supply to this argument. This is safer than using a password because
-#'   you can easily delete a PAT without affecting any others. Defaults to
-#'   the `GITHUB_PAT` environment variable.
-#' @param host GitHub API host to use. Override with your GitHub enterprise
-#'   hostname, for example, `"github.hostname.com/api/v3"`.
+#' @param pkgs One or more package names to install.
+#' @param repo Artifactory repo URL, e.g., `"company.jfrog.io/artifactory/cran-local"`.
+#' @param auth_key Artifactory API key.
 #' @param force Force installation, even if the remote state has not changed
 #'   since the previous install.
 #' @inheritParams install_deps
 #' @param ... Other arguments passed on to [utils::install.packages()].
 install_artifactory <- function(pkgs,
                                 repo = artifactory_repo(quiet),
-                                auth_user = artifactory_user(quiet),
-                                auth_key = artifactory_key(quiet),
+                                auth_token = artifactory_key(quiet),
                                 force = FALSE,
                                 dependencies = NA,
                                 upgrade = c("default", "ask", "always", "never"),
@@ -44,14 +25,10 @@ install_artifactory <- function(pkgs,
                                 type = getOption("pkgType"),
                                 ...) {
 
-  message("install_artifactory: ", paste0(pkgs, collapse = ", "))
-
-  remotes <- lapply(pkgs, artifactory_remote, repo = repo, user = auth_user, key = auth_key)
+  remotes <- lapply(pkgs, artifactory_remote, repo = repo, auth_token = auth_token)
 
   install_remotes(
     remotes,
-    host = host,
-    basic_auth = list(user = auth_user, key = auth_key),
     dependencies = dependencies,
     upgrade = upgrade,
     force = force,
@@ -65,50 +42,51 @@ install_artifactory <- function(pkgs,
     ...)
 }
 
-#' Create a new github_remote
+#' Create a new Artifactory remote
 #'
-#' This is an internal function to create a new github_remote, users should
-#' generally have no need for it.
-#' @inheritParams install_github
+#' This is an internal function to create a new `artifactory_remote`.
+#' @inheritParams install_artifactory
 #' @export
 #' @keywords internal
 artifactory_remote <- function(package,
                                repo = artifactory_repo(),
-                               user = artifactory_user(),
-                               key = artifactory_key()) {
+                               auth_token = artifactory_key()) {
 
-  message("artifactory_remote: ", package, sprintf(" (%s / %s : %s)", repo, user, key))
-
-  basic_auth <- list(user = user, password = key)
-
-  info <- artifactory_package_info(
-    pkg = package,
-    repo_url = build_url(repo, "src", "contrib"),
-    basic_auth = basic_auth
+  # Source-only for now
+  details <- artifactory_package_metadata(
+    pkgname = package,
+    repo = repo,
+    key = auth_token,
+    type = "source"
   )
 
-  message("info: ", paste0(info, collapse = " // "))
+  if (is.null(details)) {
+    stop(sprintf('Package "%s" not found in Artifactory.', package))
+  }
 
   remote(
     "artifactory",
-    package = package,
-    version = info$version,
-    host = info$url,
-    basic_auth = list(user = user, password = key)
+    package = details$properties$cran.name,
+    repo = repo,
+    url = details$downloadUri,
+    sha = details$checksums$sha1,
+    api_key = auth_token
   )
+}
+
+#' @export
+format.artifactory_remote <- function(x, ...) {
+  "Artifactory"
 }
 
 #' @export
 remote_download.artifactory_remote <- function(x, quiet = FALSE) {
   dest <- tempfile(fileext = ".tar.gz")
   if (!quiet) {
-    message(sprintf(
-      "Downloading package \"%s\" from Artifactory.\nURL: %s",
-      x$package,
-      x$host
-    ))
+    message(sprintf('Downloading package "%s" from Artifactory.', x$package))
+    message(sprintf("URL: %s", x$url))
   }
-  download(dest, x$host, basic_auth = x$basic_auth)
+  download(dest, x$url, headers = list("X-JFrog-Art-Api" = x$api_key))
 }
 
 #' @export
@@ -118,37 +96,81 @@ remote_package_name.artifactory_remote <- function(remote, ...) {
 
 #'  @export
 remote_sha.artifactory_remote <- function(remote, ...) {
-  NULL
+  remote$sha
 }
 
 #' @export
 remote_metadata.artifactory_remote <- function(x, bundle = NULL, source = NULL, sha = NULL) {
   list(
     RemoteType = "artifactory",
-    RemoteHost = x$host,
-    RemoteSha = x$version
+    RemoteRepo = x$repo,
+    RemoteSha = x$sha
   )
+}
+
+
+#' Fetch package metadata using Artifactory API.
+#'
+#' @param pkgname The remote package name.
+#' @param repo Artifactory repository URL (assumed `"<host>/<repo>"`).
+#' @param key Artifactory API key.
+#' @param type Package type (only `"source"` allowed for now).
+#'
+#' @return A list of package metadata.
+#' @keywords internal
+artifactory_package_metadata <- function(pkgname,
+                                         repo  = artifactory_repo(),
+                                         key = artifactory_key(),
+                                         type = "source") {
+
+  type <- match.arg(type)
+
+  if (!has_package("jsonlite")) {
+    stop('Package "jsonlite" is required for Artifactory remotes')
+  }
+
+  # Separate the host from the repo
+  url_parts <- unlist(strsplit(sub("^.*?://", "", repo), "/", fixed = TRUE))
+  url_host <- build_url(parts[1:(length(parts) - 1)])
+  url_repo <- tail(parts, 1)
+
+  api_url <- build_url(
+    url_host, "api", "search",
+    sprintf("artifact?name=%s&repo=%s", pkgname, url_repo)
+  )
+
+  jsonfile <- tempfile(fileext = ".json")
+
+  download(jsonfile, api_url,
+    headers = list(
+      "X-JFrog-Art-Api" = key,
+      "X-Result-Detail" = "info,properties"
+    )
+  )
+
+  payload <- jsonlite::read_json(jsonfile)[["results"]]
+
+  result <- Filter(function(x) grepl("src/contrib", x$path, fixed = TRUE), payload)
+  result <- Filter(function(x) x$properties$cran.name == pkgname, result)
+  result <- unlist(result, recursive = FALSE)
+
+  result
 }
 
 
 #' Set Artifactory environmental variables.
 #'
-#' DFGHJKL
+#' Attempt to set Artifactory URL and authentication parameters
+#' from the environment.
 #'
-#' @param quiet XXSD
-#' @describeIn artifactory_repo AAA
+#' @param quiet Do not show any status messages.
+#' @describeIn artifactory_repo Artifactory repository URL (`ARTIFACTORY_REPO`).
 #' @export
 artifactory_repo <- function(quiet = TRUE) {
   artifactory_envvar("ARTIFACTORY_REPO", "repo", quiet)
 }
 
-#' @describeIn artifactory_repo XXX
-#' @export
-artifactory_user <- function(quiet = TRUE) {
-  artifactory_envvar("ARTIFACTORY_USER", "user name", quiet)
-}
-
-#' @describeIn artifactory_repo YYY
+#' @describeIn artifactory_repo Artifactory API key (`ARTIFACTORY_KEY`).
 #' @export
 artifactory_key <- function(quiet = TRUE) {
   artifactory_envvar("ARTIFACTORY_KEY", "API key", quiet)
@@ -159,7 +181,7 @@ artifactory_key <- function(quiet = TRUE) {
 #' Adapted from [gitlab_pat()].
 #'
 #' @param key Environment variable name.
-#' @param desc Short description, e.g., "user name."
+#' @param desc Short description, e.g., "API key."
 #' @param quiet Print status message.
 #' @keywords internal
 artifactory_envvar <- function(key, desc, quiet = TRUE) {
@@ -170,34 +192,3 @@ artifactory_envvar <- function(key, desc, quiet = TRUE) {
   }
   return(NULL)
 }
-
-
-artifactory_package_info <- function(pkg, repo_url, basic_auth = NULL) {
-  repolist <- download(tempfile(), repo_url, basic_auth = basic_auth)
-  repolist <- readLines(repolist, warn = FALSE)
-
-  # Lines with the package name
-  matches <- grep(pkg, repolist, fixed = TRUE, value = TRUE)
-  matches <- gsub(paste0("^.*(", pkg, "_.*?\\.tar\\.gz).*$"), "\\1", matches, perl = TRUE)
-
-  # Check number of matches
-
-  # Get version
-  chomp <- sub(".tar.gz", "", matches, fixed = T)
-  chomp <- sub(paste0(pkg, "_"), "", chomp, fixed = T)
-
-  list(package = pkg, version = chomp, url = build_url(repo_url, matches))
-}
-
-
-
-
-
-
-
-#
-# https://elderresearch.jfrog.io/artifactory/cran-local/bin/macosx/el-capitan/contrib/3.6/erilang_0.0.0.9000.tgz
-#
-#
-
-
---
+++ tests/testthat/test-install-artifactory.R
@@ -0,0 +1,1 @@
+context("Install from Artifactory")
