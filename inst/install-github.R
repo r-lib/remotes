7873aa64bcfbcd94212086a60b84a689aa530749
Jim Hester <james.f.hester@gmail.com>
2020-09-18 13:28:01 -0400

    Add example keeping source references and tests
    Fixes #540

--- inst/install-github.R
+++ inst/install-github.R
@@ -2871,6 +2871,9 @@ function(...) {
   #' # GITHUB_PAT.
   #' install_github("hadley/private", auth_token = "abc")
   #'
+  #' # To pass option arguments to `R CDM INSTALL` use `INSTALL_opts`. e.g. to
+  #' install a package with source references and tests
+  #' install_github("rstudio/shiny", INSTALL_opts = c("--with-keep.source", "--install-tests"))
   #' }
   install_github <- function(repo,
                              ref = "HEAD",
