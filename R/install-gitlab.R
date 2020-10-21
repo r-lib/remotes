0c7642ef7f984ef435d2beb8cb60feefd47872c5
Dr. Damian Gola <d.gola@euroimmun.de>
2020-10-21 08:30:03 +0200

    Encode ref like in github_commit

--- R/install-gitlab.R
+++ R/install-gitlab.R
@@ -149,7 +149,7 @@ format.gitlab_remote <- function(x, ...) {
 gitlab_commit <- function(username, repo, ref = "HEAD",
   host = "gitlab.com", pat = gitlab_pat()) {
 
-  url <- build_url(host, "api", "v4", "projects", utils::URLencode(paste0(username, "/", repo), reserved = TRUE), "repository", "commits", ref)
+  url <- build_url(host, "api", "v4", "projects", utils::URLencode(paste0(username, "/", repo), reserved = TRUE), "repository", "commits", utils::URLencode(ref, reserved = TRUE))
 
   tmp <- tempfile()
   download(tmp, url, headers = c("Private-Token" = pat))
