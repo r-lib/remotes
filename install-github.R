a7dac934c58ac6616e15b136f64492be8cec746f
Kirill Müller <krlmlr+r@mailbox.org>
2021-01-24 07:40:06 +0100

    Sync

--- install-github.R
+++ install-github.R
@@ -2497,10 +2497,13 @@ function(...) {
   # Contents of R/install-dev.R
   #' Install the development version of a package
   #'
+  #' @description
   #' `install_dev()` retrieves the package DESCRIPTION from the CRAN mirror and
   #' looks in the 'URL' and 'BugReports' fields for GitHub, GitLab or Bitbucket
   #' URLs. It then calls the appropriate `install_()` function to install the
   #' development package.
+  #'
+  #' `can_install_dev()` checks if the development version can be installed.
   #'
   #' @param package The package name to install.
   #' @param cran_url The URL of the CRAN mirror to use, by default based on the
@@ -2509,6 +2512,7 @@ function(...) {
   #'   [install_gitlab()], or [install_bitbucket()] functions.
   #' @family package installation
   #' @export
+  #' @name install_dev
   #' @examples
   #' \dontrun{
   #' # From GitHub
@@ -2520,7 +2524,7 @@ function(...) {
   #' # From Bitbucket
   #' install_dev("argparser")
   #' }
-  install_dev <- function(package, cran_url = getOption("repos")[["CRAN"]], ...) {
+  install_dev <- function(package, cran_url = getOption("repos")[["CRAN"]]) {
     if (is.null(cran_url) || identical(cran_url, "@CRAN@")) {
       cran_url <- "https://cloud.r-project.org"
     }
@@ -2537,7 +2541,7 @@ function(...) {
     url_fields <- c(desc$URL, desc$BugReports)
   
     if (length(url_fields) == 0) {
-      stop("Could not determine development repository", call. = FALSE)
+      return(FALSE)
     }
   
     pkg_urls <- unlist(strsplit(url_fields, "[[:space:]]*,[[:space:]]*"))
@@ -2561,7 +2565,13 @@ function(...) {
   
     parts <- unique(stats::na.omit(parts))
   
-    if (nrow(parts) != 1) {
+    # Can install if we have exactly one valid part
+    nrow(parts) == 1
+  }
+  
+  #' @export
+  install_dev <- function(package, cran_url = getOption("repos")[["CRAN"]], ...) {
+    if (!can_install_dev(package, cran_url)) {
       stop("Could not determine development repository", call. = FALSE)
     }
   
