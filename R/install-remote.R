r-lib/remotes

commit 62ff718d479c17cf9e9fd4b9867246f8ea9e2428

Support local remotes installed by pak
pak local remotes don't have a RemoteType field, but do have a
ReomtePkgRef field, so we check for this when creating local remotes

R/install-remote.R
@@ -274,7 +274,12 @@ package2remote <- function(name, lib = .libPaths(), repos = getOption("repos"), 
       revision = x$RemoteSha,
       args = x$RemoteArgs),
     local = remote("local",
-      path = trim_ws(x$RemoteUrl),
+      path = {
+        path <- trim_ws(x$RemoteUrl)
+        if (length(path) == 0) {
+          path <- parse_pkg_ref(x$RemotePkgRef)$ref
+        }
+      },
       subdir = x$RemoteSubdir,
       sha = {
         # Packages installed locally might have RemoteSha == NA_character_
@@ -299,6 +304,14 @@ package2remote <- function(name, lib = .libPaths(), repos = getOption("repos"), 
       branch = x$RemoteBranch),
     stop(sprintf("can't convert package %s with RemoteType '%s' to remote", name, x$RemoteType))
   )
+}
+
+parse_pkg_ref <- function(x) {
+  res <- re_match(x, "(?<type>[^:]+)::(?<ref>.*)")
+  if (is.na(res$ref)) {
+    stop("Invalid package reference:\n  ", x, call. = FALSE)
+  }
+  res
 }
 
 #' @export

inst/install-github.R
@@ -3682,7 +3682,12 @@ function(...) {
         revision = x$RemoteSha,
         args = x$RemoteArgs),
       local = remote("local",
-        path = trim_ws(x$RemoteUrl),
+        path = {
+          path <- trim_ws(x$RemoteUrl)
+          if (length(path) == 0) {
+            path <- parse_pkg_ref(x$RemotePkgRef)$ref
+          }
+        },
         subdir = x$RemoteSubdir,
         sha = {
           # Packages installed locally might have RemoteSha == NA_character_
@@ -3707,6 +3712,14 @@ function(...) {
         branch = x$RemoteBranch),
       stop(sprintf("can't convert package %s with RemoteType '%s' to remote", name, x$RemoteType))
     )
+  }
+  
+  parse_pkg_ref <- function(x) {
+    res <- re_match(x, "(?<type>[^:]+)::(?<ref>.*)")
+    if (is.na(res$ref)) {
+      stop("Invalid package reference:\n  ", x, call. = FALSE)
+    }
+    res
   }
   
   #' @export

install-github.R
@@ -3682,7 +3682,12 @@ function(...) {
         revision = x$RemoteSha,
         args = x$RemoteArgs),
       local = remote("local",
-        path = trim_ws(x$RemoteUrl),
+        path = {
+          path <- trim_ws(x$RemoteUrl)
+          if (length(path) == 0) {
+            path <- parse_pkg_ref(x$RemotePkgRef)$ref
+          }
+        },
         subdir = x$RemoteSubdir,
         sha = {
           # Packages installed locally might have RemoteSha == NA_character_
@@ -3707,6 +3712,14 @@ function(...) {
         branch = x$RemoteBranch),
       stop(sprintf("can't convert package %s with RemoteType '%s' to remote", name, x$RemoteType))
     )
+  }
+  
+  parse_pkg_ref <- function(x) {
+    res <- re_match(x, "(?<type>[^:]+)::(?<ref>.*)")
+    if (is.na(res$ref)) {
+      stop("Invalid package reference:\n  ", x, call. = FALSE)
+    }
+    res
   }
   
   #' @export

commit 1a76c9c0691a3983432d20e2d15a75d3fb64d54e

Fix local remote path
We need to return the value in both cases. This fixes a bug introduced
by commit 62ff718d479c17cf9e9fd4b9867246f

R/install-remote.R
@@ -279,6 +279,7 @@ package2remote <- function(name, lib = .libPaths(), repos = getOption("repos"), 
         if (length(path) == 0) {
           path <- parse_pkg_ref(x$RemotePkgRef)$ref
         }
+        path
       },
       subdir = x$RemoteSubdir,
       sha = {