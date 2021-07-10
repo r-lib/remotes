37e177b049fe045ec6aa3f5c538b2e159e8629dc
dgkf <18220321+dgkf@users.noreply.github.com>
2021-07-09 18:31:40 -0700

    updating DESCRIPTION; tests

--- tests/testthat/test-install-gitlab.R
+++ tests/testthat/test-install-gitlab.R
@@ -180,7 +180,7 @@ test_that("gitlab_remote reverts to git2r_remote when git_fallback with git2r", 
 
 test_that("gitlab_remote reverts to xgit_remote when git_fallback and no git2r", {
   withr::local_envvar(c(GITLAB_PAT=""))
-  mockery::stub(gitlab_to_git_remote, "pkg_installed", function(...) FALSE, 2L)  # assume git2r unavailable
+  mockery::stub(gitlab_remote, "pkg_installed", function(...) FALSE, 2L)  # assume git2r unavailable
   
   expect_message({
     r <- gitlab_remote(
