43451823ff6113278ef055b1f19053db2452f1b3
John Muschelli <muschellij2@gmail.com>
2021-12-02 20:58:19 +0000

    added .com

--- R/install-git.R
+++ R/install-git.R
@@ -264,7 +264,7 @@ remote_download.xgit_remote <- function(x, quiet = FALSE) {
 
   bundle <- tempfile()
 
-  disallows_unadvertised <- grepl("github|bitbucket", tolower(x$url))
+  disallows_unadvertised <- grepl("(github|bitbucket)[.]com", tolower(x$url))
   args <- c("clone", if (is.null(x$ref) && disallows_unadvertised) c("--depth", "1"), "--no-hardlinks")
   args <- c(args, x$args, x$url, bundle)
   git(paste0(args, collapse = " "), quiet = quiet)
