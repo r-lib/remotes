76ccfb2828678c5419c21bf40b72cb15334ae490
Eli Holmes <eli.holmes@noaa.gov>
2024-11-21 18:14:01 -0800

    respect auth_token=NULL

--- R/install-github.R
+++ R/install-github.R
@@ -260,7 +260,7 @@ remote_package_name.github_remote <- function(remote, ..., use_local = TRUE,
 remote_sha.github_remote <- function(remote, ..., use_curl = !is_standalone() && pkg_installed("curl")) {
   tryCatch(
     github_commit(username = remote$username, repo = remote$repo,
-      host = remote$host, ref = remote$ref, pat = remote$auth_token %||% github_pat(), use_curl = use_curl),
+      host = remote$host, ref = remote$ref, pat = remote$auth_token, use_curl = use_curl),
 
     # 422 errors most often occur when a branch or PR has been deleted, so we
     # ignore the error in this case
