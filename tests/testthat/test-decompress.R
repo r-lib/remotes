c228c881dc255650c2143500e317e073685421e7
Jacci Ziebert <jacciziebert@gmail.com>
2026-09-17 10:52:47 -0500

    replace mockery with local_mocked_bindings

--- tests/testthat/test-decompress.R
+++ tests/testthat/test-decompress.R
@@ -19,31 +19,29 @@ test_that("decompress various file types", {
 
 test_that("decompress with internal unzip", {
 
+  local_mocked_bindings(
+    getOption = function(x, default = NULL) {
+      if (x == "unzip") {
+        "internal"
+      } else {
+        if (missing(default) || x %in% names(options())) {
+          options()[[x]]
+        } else {
+          default
+        }
+      }
+    },
+    .package = "base"
+  )
+
   types <- c("zip", "tar", "tar.gz", "tgz")
 
   for (type in types) {
-
     fname <- paste0("foo.", type)
     archive <- file.path("archives", fname)
 
     dec <- tempfile()
     on.exit(unlink(dec, recursive = TRUE), add = TRUE)
-
-    mockery::stub(
-      decompress,
-      "getOption",
-      function(x, default = NULL) {
-        if (x == "unzip") {
-          "internal"
-        } else {
-          if (missing(default) || x %in% names(options())) {
-            options()[[x]]
-          } else {
-            default
-          }
-        }
-      }
-    )
 
     decompress(archive, dec)
 
